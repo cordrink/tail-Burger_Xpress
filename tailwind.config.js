a8479d2a89a42c88645da194120907a38d3c0f4b
cordrink <masodavictorjudicael@gmail.com>
2024-10-23 19:39:22 +0100

    Realisation de section hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#E7272D",
-        secondary: "#FDB217",
-      },
-      backgroundImage: {
-        "ardoise": "url('images/background-slate.jpg')"
-      }
+    content: ["./public/**/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#E7272D",
+                secondary: "#FDB217",
+            },
+            backgroundImage: {
+                "ardoise": "url('images/background-slate.jpg')"
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
